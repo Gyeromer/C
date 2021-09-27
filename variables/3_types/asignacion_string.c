ba0f5d0d501d39e9f634a99be920e690d23463c4
GerardoDLR <gerardo_lara@comunidad.unam.mx>
2021-09-27 11:43:53 -0500

    C
    clase

--- variables/3_types/asignacion_string.c
+++ variables/3_types/asignacion_string.c
@@ -3,10 +3,16 @@
 #include <string.h> // Archivo de cabecera para funciones cadena
 int main()
 { // Inicio
- char saludo[20] = "Hola"; // Define variable cadena
- strcpy(saludo,"Buenos"); // Asigna valor a cadena
- strcat(saludo," dias");
- strcat(saludo,"_usuario");
- printf("%s\n",saludo); // Muestra cadena nueva
+char nombre[50] = "Gerardo De La Rosa Lara";
+ //char saludo[20] = "Hola"; // Define variable cadena
+ //char pais[3] = "MXN"; 
+ 
+ //char apellidopaterno[50] = "";
+ //char apellidomaterno[50] = "";
+ //strcpy(saludo,"Buenos"); // Asigna valor a cadena
+ //strcat(saludo," dias");
+ //strcat(saludo,"_usuario");
+// printf("%s\n", pais); // Muestra cadena nueva
+printf("\n%s\n",nombre);
  return 0;
 } // Fin de programa
